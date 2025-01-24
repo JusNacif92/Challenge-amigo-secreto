827ecd96a2f103f3948975d71da82aea6ec78d75
Jused Nacif <jusedhng92@gmail.com>
2025-01-24 16:43:05 -0600

    Actualiza el app.js

--- apps.js
+++ apps.js
@@ -1,8 +1,18 @@
 //Declaracion de Variables
 let amigos = [];
 
-function añadirAmigo() {
+alert('hota');
+
+//Esta funcion espara agregar amigos al juego
+function agregarAmigo() {
+    //capturar el valor de campo de entrada
     let nombreUsuario = (document.getElementById("text").value);
+    //Validacion de entrada de valor
+    if(nombreUsuario == null){
+        alert('Porfavor digite un nombre');
+    }
 
     
 }
+
+agregarAmigo();
