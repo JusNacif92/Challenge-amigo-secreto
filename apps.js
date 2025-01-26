60de71c7e95147e35cd6d2a0e6497ff87cf6ec2b
Jused Nacif <jusedhng92@gmail.com>
2025-01-25 19:43:31 -0600

    Actualiza apps

--- apps.js
+++ apps.js
@@ -1,20 +1,22 @@
 //Declaracion de Variables
 let amigos = [];
-
-
-console.log("hola");
-
 //Esta funcion espara agregar amigos al juego
 function agregarAmigo() {
     //capturar el valor de campo de entrada
     let nombreUsuario = document.getElementById('amigo').value;
-    console.log (nombreUsuario);
+    //console.log (nombreUsuario);
     //Validacion de entrada de valor
-    if(amigos == " "){
+    //console.log(typeof(nombreUsuario));
+    if(nombreUsuario === "" || nombreUsuario == " " || !isNaN(nombreUsuario)){
+        /* El if valida que el campo no se quede vacio o que solo digite una cadena de texto o que no
+        ingrese numeros.*/
         alert('Porfavor, inserte un nombre');
-    }
+    }else{
+        //Actualiozar el array si el valor es valido.
+        amigos.push (nombreUsuario);//Guarada el nombre en el array amigos
+        console.log (amigos);//Se guarda en la consola
 
-    
+    } 
 }
 
 agregarAmigo();
